933473da9f1237e96fc639a99d858d09d66cd708
Дёшин Михаил <m.deshin@vniias.ru>
2026-08-13 17:29:13 +0300

    Refactored the jumping-logic: now jumping charge initially is 1 and there is a clear charging-jump state

--- player.gd
+++ player.gd
@@ -1,42 +1,43 @@
 extends CharacterBody2D
 
+@export var jump_velocity := -400.0
+@export var horiz_speed := 200.0
+@export var deceleration := 10.0
+@export var jump_charge_rate := 4.0
 
+const MAX_JUMP_CHARGE := 2.0
+const INIT_JUMP_CHARGE := 1.0
 
-@export var jump_velocity = -400.0
-@export var horiz_speed = 200.0
-@export var deceleration = 10.0
+var jump_charge := INIT_JUMP_CHARGE
+var is_charging_jump := false
 
-const JUMP_POWER_LIMIT = 2.0
-var jump_power = 0.0
-@export var jump_power_step = 8
 
 func _physics_process(delta: float) -> void:
-	
-	
-	# Add the gravity.
+
+	# Gravity
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
-	# Handle jump.
-	#if Input.is_action_just_pressed("jump") and is_on_floor():
-		#velocity.y = jump_velocity
+	# Start charging
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		is_charging_jump = true
+		jump_charge = INIT_JUMP_CHARGE
 		
-	if Input.is_action_pressed("jump") and is_on_floor():
-		jump_power += jump_power_step * delta 
-		
-		if jump_power > JUMP_POWER_LIMIT:
-			jump_power = JUMP_POWER_LIMIT
-		
-		
-		
-	if Input.is_action_just_released("jump") and is_on_floor():
-		print(jump_power)
-		velocity.y = jump_power * jump_velocity
-		jump_power = 0
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
+	# Charge
+	if Input.is_action_pressed("jump") and is_charging_jump:
+		jump_charge += jump_charge_rate * delta
+		jump_charge = min(jump_charge, MAX_JUMP_CHARGE)
+
+	# Release
+	if Input.is_action_just_released("jump") and is_charging_jump:
+		velocity.y = jump_charge * jump_velocity
+		jump_charge = INIT_JUMP_CHARGE
+		is_charging_jump = false
+
+	# Horizontal movement
 	var direction := Input.get_axis("move_left", "move_right")
+
 	if direction:
 		velocity.x = direction * horiz_speed
 	else:
