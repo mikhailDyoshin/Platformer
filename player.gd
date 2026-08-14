c9301a483d30fc7f766cb3ef8322f952d63568a0
Дёшин Михаил <m.deshin@vniias.ru>
2026-08-14 10:45:44 +0300

    Added more tiles. Now player falls off if there's no floor under him

--- player.gd
+++ player.gd
@@ -3,10 +3,12 @@ extends CharacterBody2D
 @export var jump_velocity := -400.0
 @export var horiz_speed := 200.0
 @export var deceleration := 10.0
-@export var jump_charge_rate := 4.0
+@export var acceleration := 10.0
+@export var jump_charge_rate := 2.0
 
 const MAX_JUMP_CHARGE := 2.0
 const INIT_JUMP_CHARGE := 1.0
+const MAX_WALKING_SPEED := 200.0
 
 var jump_charge := INIT_JUMP_CHARGE
 
@@ -51,6 +53,10 @@ func idle(delta):
 		state = State.CHARGING
 		jump_charge = INIT_JUMP_CHARGE
 		print_state()
+		return
+	
+	if not is_on_floor():
+		fall()
 		
 func walking(delta):
 	var direction := Input.get_axis("move_left", "move_right")
@@ -60,19 +66,22 @@ func walking(delta):
 		print_state()
 		return
 		
-	velocity.x = direction * horiz_speed
+	accelerate_walk(direction)
 	
 	if Input.is_action_just_pressed("jump"):
 		state = State.CHARGING
 		jump_charge = INIT_JUMP_CHARGE
 		print_state()
+		
+	if not is_on_floor():
+		fall()
 	
 func jumping(delta: float):
 	velocity += get_gravity() * delta
 	var direction = Input.get_axis("move_left", "move_right")
 	
 	if direction:
-		velocity.x = direction * horiz_speed
+		accelerate_walk(direction)
 	else:
 		velocity.x = move_toward(velocity.x, 0, deceleration)
 		
@@ -88,7 +97,8 @@ func jumping(delta: float):
 	#pass
 	
 func charging(delta):
-	velocity.x = move_toward(velocity.x, 0, deceleration)
+	var direction := Input.get_axis("move_left", "move_right")
+	accelerate_walk(direction)
 	# Charge
 	jump_charge += jump_charge_rate * delta
 	jump_charge = min(jump_charge, MAX_JUMP_CHARGE)
@@ -104,3 +114,18 @@ func get_state_name(state: State) -> String:
 	
 func print_state():
 	print(get_state_name(state))
+ 
+func accelerate_walk(direction):
+	var data = {"v": velocity.x, "max_v": direction * MAX_WALKING_SPEED, "a": direction * acceleration}
+	var text = "v: {v}\nmax_v: {max_v}\na: {a}".format(data)
+	print(data)
+	velocity.x = direction * move_toward(abs(velocity.x), MAX_WALKING_SPEED, acceleration)
+
+func decelerate():
+	velocity.x = move_toward(velocity.x, 0, deceleration)
+
+func fall():
+	state = State.JUMPING
+	jump_charge = INIT_JUMP_CHARGE
+	print_state()
+	return
